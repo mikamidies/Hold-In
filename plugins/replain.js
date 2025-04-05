87e397e1c904d03212daf50a2fee5039763c5e21
Davronali <davronali84@gmail.com>
2025-04-05 19:15:39 +0500

    replain bot added

--- plugins/replain.js
+++ plugins/replain.js
@@ -1,10 +1,11 @@
 export default defineNuxtPlugin(() => {
-  window.replainSettings = { id: "d9ff733b-8898-4795-8869-e282c8281f96" };
+  window.replainSettings = { id: "5193e600-51e4-4624-aa95-c8212628e345" };
+
   (function (u) {
-    var s = document.createElement("script");
+    const s = document.createElement("script");
     s.async = true;
     s.src = u;
-    var x = document.getElementsByTagName("script")[0];
+    const x = document.getElementsByTagName("script")[0];
     x.parentNode.insertBefore(s, x);
   })("https://widget.replain.cc/dist/client.js");
 });
